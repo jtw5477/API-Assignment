83a9699f39a35e7f9da6eae7ad17c302e6501d88
jtw5477 <70678051+jtw5477@users.noreply.github.com>
2021-02-15 21:07:55 -0500

    Initial Commit.

---
+++ API.java
@@ -0,0 +1,34 @@
+package com.example.api;
+import org.json.JSONObject;
+
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.*;
+/*
+ * This class calls a Hearthstone API to gather up to date
+ * information about cards, classes and sets from the game
+ * Updated on 2/15/2021
+ * @author jtw5477 Jayson Williamson
+ */
+
+
+public class API {
+    public static void getHearthstoneInfo(){
+        try {
+            //Connects to the desired endpointS
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create("https://omgvamp-hearthstone-v1.p.rapidapi.com/info"))
+                    .header("x-rapidapi-key", "4d8bcf4474msh9e0ef762eb76f3bp1886d1jsn8339e1ed14cc")
+                    .header("x-rapidapi-host", "omgvamp-hearthstone-v1.p.rapidapi.com")
+                    .method("GET", HttpRequest.BodyPublishers.noBody())
+                    .build();
+            HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+            //Print out all the info available currently in game about cards, classes, sets etc.
+            System.out.println(response.body());
+
+        } catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }
+    }
+}
---
+++ Main.java
@@ -0,0 +1,8 @@
+package com.example.api;
+
+public class Main {
+    public static void main(String[] args){
+        API hearthstone = new API();
+        hearthstone.getHearthstoneInfo();
+    }
+}
